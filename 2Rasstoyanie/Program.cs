71c43b66ceb30d2a2310bbffca3c98daba34ba1e
Sasha <efremovasasha@rambler.ru>
2023-07-14 01:10:27 +0300

    .\2Rasstoyanie\

---
+++ 2Rasstoyanie/Program.cs
@@ -0,0 +1,21 @@
+﻿// Принимает координаты точек и выдает расстояние между ними d ghjcnhfycndt.
+double Distance (int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    double result = Math.Sqrt((x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1)+(z2 - z1)*(z2 - z1));
+    return result;
+}
+
+Console.Write("x1: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y1: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z1: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("x2: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("y2: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("z2: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Distance(x1, x2, y1, y2, z1, z2));

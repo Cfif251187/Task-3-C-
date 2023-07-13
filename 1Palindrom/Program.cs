8718c3b12cd433c946df54cf9c5449ef505be0ae
Sasha <efremovasasha@rambler.ru>
2023-07-14 01:05:50 +0300

    .\1Palindrom\

---
+++ 1Palindrom/Program.cs
@@ -0,0 +1,18 @@
+﻿// Программа проверяет, является ли число палиндромом.
+bool Palindrome(int num)
+{
+   string number = num.ToString();
+   for(int i = 0; i < number.Length/2; i++)
+{
+    if(number[i] != number[number.Length-i-1])
+    {
+        return false;
+    }
+}
+return true;
+}
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool result = Palindrome(number);
+Console.WriteLine(result);

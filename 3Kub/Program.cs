b1d29b577e880bd855f9080f8ee8278ac2791504
Sasha <efremovasasha@rambler.ru>
2023-07-14 00:00:42 +0300

    .\3Kub\

---
+++ 3Kub/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа принимает число и выводит таблицу кубов.
+void Cube(int cub)
+{
+    int count = 1;
+    while(count<= cub)
+    {
+        Console.WriteLine($"{count} - {count*count*count}");
+        count++;
+    }
+}
+Console.Write("Введите число: ");
+int cub = Convert.ToInt32(Console.ReadLine());
+
+Cube (cub);
